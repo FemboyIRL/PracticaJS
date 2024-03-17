bc758473fe80cd96a5683f4885651fca251b2d22
FemboyIRL <gatoelluis@gmail.com>
2024-03-17 15:42:28 -0700

    PracticaJS 1.0

---
+++ script.js
@@ -0,0 +1,31 @@
+const textTitulo = document.querySelector('.infoTitulo')
+const textSubtitulo = document.querySelector('.infoSubtitulo')
+const textDescripcion = document.querySelector('.infoParrafo')
+const inputTitulo = document.querySelector('#formTitulo')
+const inputSubtitulo = document.querySelector('#formSubtitulo')
+const inputDescripcion = document.querySelector('#formDescripcion')
+const formCourse = document.querySelector('#formCourse')
+const cajaInfo = document.querySelector('.cajaInfo')
+const imgMin = document.querySelector('.imgMin')
+const center1 = document.querySelector('.center1')
+
+//-- FUNCIONES --//
+
+function actualizarCurso(e){
+    e.preventDefault();
+    textTitulo.querySelector('h1').innerText = inputTitulo.value;
+    textSubtitulo.querySelector('h2').innerText = inputSubtitulo.value;
+    textDescripcion.querySelector('p').innerText = inputDescripcion.value;
+    this.reset();
+
+}
+
+
+function actualizarImagen(e){
+    center1.style.backgroundImage = `url('${e.target.src}')`;
+}
+
+//-- EVENTOS --//
+
+formCourse.addEventListener('submit', actualizarCurso);
+imgMin.addEventListener('click', actualizarImagen);
